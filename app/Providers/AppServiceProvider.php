1fe801cd14ce7f13757012c7094f5f2adde13e80
ıɟoɹ ɹnpqɐ | @mrofisr | mrofisr.dev <31870041+mrofisr@users.noreply.github.com>
2022-08-26 19:34:12 +0700

    update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-
+use \Illuminate\Support\Facades\URL;
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -24,5 +24,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+    }
+    if ($this->app->environment('production')) {
+        URL::forceScheme('https');
     }
 }
