6360651f3381984209f07153d5d89100187425ab
ıɟoɹ ɹnpqɐ | @mrofisr | mrofisr.dev <31870041+mrofisr@users.noreply.github.com>
2022-09-02 17:17:05 +0700

    update

--- resources/views/admin/report.blade.php
+++ resources/views/admin/report.blade.php
@@ -13,7 +13,7 @@
             <main class="h-full overflow-y-auto">
                 <div class="container px-6 mx-auto grid">
                     <h2 class="my-6 text-2xl font-semibold text-gray-700 dark:text-gray-200">
-                        User Report
+                        Hasil Test
                     </h2>
                     <div class="w-full overflow-x-auto">
                         <livewire:reports-table searchable="nama, result" />
